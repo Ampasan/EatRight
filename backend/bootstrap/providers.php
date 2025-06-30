edde55ae6f78df217592075a70d8ba1cb68030ce
Ampasan <n.rakan.r.2005@gmail.com>
2025-07-01 02:54:53 +0700

    Program modularization structure

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
+    App\Providers\AiServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthServiceProvider::class,
+    App\Providers\CloudinaryServiceProvider::class,
+    App\Providers\EventServiceProvider::class,
+    App\Providers\PaymentServiceProvider::class,
+    App\Providers\RouteServiceProvider::class,
 ];
