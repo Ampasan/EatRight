b30154a2b876fc58704a24fe52fe808a403ce4ad
Ampasan <n.rakan.r.2005@gmail.com>
2025-07-01 23:02:22 +0700

    add request

--- backend/app/Http/Requests/Auth/ChangePasswordRequest.php
+++ backend/app/Http/Requests/Auth/ChangePasswordRequest.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ChangePasswordRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
     public function authorize(): bool
     {
         return true;
@@ -14,7 +17,7 @@ class ChangePasswordRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string,
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
